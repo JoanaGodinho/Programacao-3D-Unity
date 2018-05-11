be51ca3c5407b1d64495367d8d603a9649504f02
André Lourenço <alourenco.94@hotmail.com>
2018-05-11 15:05:36 +0100

    First Person Camera Rotation
    - Camera direction is now controlled with mouse movement.

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -2,18 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
-    public float sensibilityH = 10f;
-    public float sensibilityV = 10f;
+    public float sensitivityH = 2f;
+    public float sensitivityV = 2f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateCamera();
+    }
+
+    private void rotateCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        float rotAmountH = mouseX * sensitivityH;
+        float rotAmountV = mouseY * sensitivityV;
+
+        Vector3 targetRot = transform.rotation.eulerAngles;
+        targetRot.x += -rotAmountV;
+        targetRot.y += rotAmountH;
+
+        transform.rotation = Quaternion.Euler(targetRot);
+
+    }
 }
