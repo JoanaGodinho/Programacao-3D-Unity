93db263dd73948ef70ed3e57e57fde2d4d661a34
André Lourenço <alourenco.94@hotmail.com>
2018-05-11 15:38:33 +0100

    Camera Doesn't Turn Upside Down

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,8 +7,9 @@ public class PlayerController : MonoBehaviour
 
     public float sensitivityH = 2f;
     public float sensitivityV = 2f;
-
     public Camera playerCamera;
+
+    private float xRotClamp = 0.0f;
 
     // Use this for initialization
     void Start()
@@ -32,10 +33,26 @@ public class PlayerController : MonoBehaviour
         float rotAmountH = mouseX * sensitivityH;
         float rotAmountV = mouseY * sensitivityV;
 
+        xRotClamp -= rotAmountV;
+
         Vector3 cameraRot = playerCamera.transform.rotation.eulerAngles;
         Vector3 bodyRot = transform.rotation.eulerAngles;
+
         cameraRot.x -= rotAmountV;
+        cameraRot.z = 0f;               // Avoid camera going upside down        
+
         bodyRot.y += rotAmountH;
+
+        if (xRotClamp > 90)
+        {
+            xRotClamp = 90;
+            cameraRot.x = 90;
+        }
+        else if (xRotClamp < -90)
+        {
+            xRotClamp = -90;
+            cameraRot.x = 270;
+        }
 
         playerCamera.transform.rotation = Quaternion.Euler(cameraRot);
         transform.rotation = Quaternion.Euler(bodyRot);
