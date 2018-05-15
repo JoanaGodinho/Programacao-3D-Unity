1613358414cd52545c0f9e8be580c1de98c5ebc6
André Lourenço <alourenco.94@hotmail.com>
2018-05-15 18:18:02 +0100

    Implemented Spawner
    - Spawner class spawns Spawnable objects.

---
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public Spawnable spawnObject;
+    public int maxObjects = 5;
+    public int spawnTimeGap = 10;     // seconds
+
+    private int objectsCounter = 0;
+    private float elapsedTime = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        elapsedTime += Time.deltaTime;
+
+        if (objectsCounter < maxObjects)
+        {
+            if (elapsedTime > spawnTimeGap)
+            {
+                Spawn();
+                elapsedTime = 0;
+            }
+        }
+
+	}
+
+    public void OnObjectDestroyed()
+    {
+        objectsCounter--;
+    }
+
+    private void Spawn()
+    {
+        Spawnable obj = Instantiate(spawnObject, transform.position, transform.rotation);
+        obj.setSpawner(this);
+        objectsCounter++;
+    }
+}
