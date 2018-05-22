5034e2b3e461c8fd523e6e45f7cc9243095409b4
André Lourenço <alourenco.94@hotmail.com>
2018-05-22 17:18:43 +0100

    Kills Objects in Enemy Layer

---
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public int fireRate = 1;
+    public int damage = 10;
+    public int range = 50;
+    public Camera camera;
+
+    private int layerMask = 1;
+
+	// Use this for initialization
+	void Start () {
+        // Add Enemy Layer
+        layerMask = layerMask << 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            // If hit
+            if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range, layerMask))
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+    }
+}
