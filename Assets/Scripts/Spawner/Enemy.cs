ed6e51db7bb62ea9887307204bcd4e77ac3404fb
jpmcrespo <jpmcrespo@gmail.com>
2018-05-27 05:07:28 +0100

    zombies are now fully animated

--- Assets/Scripts/Spawner/Enemy.cs
+++ Assets/Scripts/Spawner/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
 
-    public float speed;
+    public float speed, timeToAttack, attackTimer;
     public int damage = 10;
     public int seekDistance = 10;
     public int HP = 50;
@@ -14,29 +14,69 @@ public class Enemy : MonoBehaviour {
     private Spawner spawner;
     private Seek seek;
     private GameObject target;
-
+    Animator anim;
+    
     // Use this for initialization
     void Awake ()
     {
         nav = GetComponent<NavMeshAgent>();
-	}
+        anim = GetComponent<Animator>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
         nav.SetDestination(target.transform.position);
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+        if (distance < 3)
+        {
+            attackTimer += Time.deltaTime;
+        }
+        else if (attackTimer > 0)
+        {
+            attackTimer -= Time.deltaTime * 2;
+        }
+        else
+        {
+            attackTimer = 0;
+        }
 	}
 
+    bool Attack()
+    {
+        if (attackTimer > timeToAttack)
+        {
+            attackTimer = 0;
+            return true;
+        }
+        return false;
+    }
     private void OnDestroy()
     {
         spawner.OnObjectDestroyed();
     }
-    
+
+    void OnCollistionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Attack())
+            {
+                //collision.collider.SendMessageUpwards("dealDamage", this.damage, SendMessageOptions.RequireReceiver);
+            }
+                
+
+        }
+    }
+
     public int getHit(int dmg)
     {
         HP -= dmg;
-        print(HP);
         if (HP <= 0) {
-            Destroy(this.gameObject);
+            nav.isStopped = true;
+            anim.SetBool("isDead", true);
+            GetComponent<CapsuleCollider>().isTrigger = true;
+            Destroy(this.gameObject, 4);
             return 1;
         }
         return 0;
